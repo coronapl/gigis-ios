a7dbdff84947aa341fa787308f04b6952e10be05
coronapl <59809785+coronapl@users.noreply.github.com>
2021-05-01 14:04:25 -0500

    Initial Commit

---
+++ gigis-ios/gigis_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  gigis_iosApp.swift
+//  gigis-ios
+//
+//  Created by Pablo V on 01/05/21.
+//
+
+import SwiftUI
+
+@main
+struct gigis_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
