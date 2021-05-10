150775ac24343866d5e811e3e7cea22329e12485
Pablo V <7637672-coronav@users.noreply.gitlab.com>
2021-05-10 16:38:03 -0500

    Create utils: global variables for colors and for network errors.

---
+++ gigis-ios/Utils/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  gigis-ios
+//
+//  Created by Pablo V on 04/05/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Colors {
+    static var blue = Color(red: 72 / 255, green: 140 / 255, blue: 203 / 255)
+    static var orange = Color(red: 245 / 250, green: 130 / 255, blue: 32 / 255)
+    static var green = Color(red: 122 / 250, green: 193 / 255, blue: 67 / 255)
+    static var yellow = Color(red: 255 / 250, green: 201 / 255, blue: 7 / 255)
+    static var lightGrey = Color(red: 239 / 255, green: 243 / 255, blue: 244 / 255)
+}
